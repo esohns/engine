48d4dc5d4d5458125b9e06e08621754a3e566a86
Erik Sohns <erik.sohns@posteo.de>
2023-06-17 11:46:36 +0200

    minor changes

--- pge_34.cpp
+++ pge_34.cpp
@@ -35,14 +35,17 @@ PGE_34::OnUserUpdate (float fElapsedTime)
   const int total = 200;
   factor_ += 0.015f;
 
-  olc::PixelGameEngine::DrawCircle ({olc::PixelGameEngine::ScreenWidth () / 2, olc::PixelGameEngine::ScreenHeight () / 2}, static_cast<int32_t> (r_), olc::WHITE, 0xFF);
+  int half_w = olc::PixelGameEngine::ScreenWidth () / 2;
+  int half_h = olc::PixelGameEngine::ScreenHeight () / 2;
+
+  olc::PixelGameEngine::DrawCircle ({half_w, half_h}, static_cast<int32_t> (r_), olc::WHITE, 0xFF);
   olc::vd2d a, b;
   for (int i = 0; i < total; i++)
   {
     a = getVector (i, total);
-    a += {static_cast<float> (olc::PixelGameEngine::ScreenWidth () / 2.0f), static_cast<float> (olc::PixelGameEngine::ScreenHeight () / 2.0f)};
+    a += {static_cast<float> (half_w), static_cast<float> (half_h)};
     b = getVector (static_cast<int> (i * factor_), total);
-    b += {static_cast<float> (olc::PixelGameEngine::ScreenWidth () / 2.0f), static_cast<float> (olc::PixelGameEngine::ScreenHeight () / 2.0f)};
+    b += {static_cast<float> (half_w), static_cast<float> (half_h)};
     olc::PixelGameEngine::DrawLine (a, b, olc::WHITE, 0xFFFFFFFF);
   } // end FOR
 
@@ -54,7 +57,7 @@ PGE_34::getVector (int index_in, int total_in)
 {
   float angle_f =
     Common_GL_Tools::map (static_cast<float> (index_in % total_in), 0.0f, static_cast<float> (total_in), 0.0f, static_cast<float> (M_PI) * 2.0f);
-  angle_f += static_cast<float> (M_PI);
+  //angle_f += static_cast<float> (M_PI);
   olc::vf2d v = {cos (angle_f), sin (angle_f)};
   v *= r_;
   return v;
