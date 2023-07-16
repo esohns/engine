ddbf819420a883932931b4cb92ff65777766ef73
Erik Sohns <erik.sohns@posteo.de>
2023-07-16 17:39:52 +0200

    minor changes

--- pge_56.cpp
+++ pge_56.cpp
@@ -28,7 +28,7 @@ PGE_56::OnUserUpdate (float fElapsedTime)
 
   for (int32_t y = 100; y < olc::PixelGameEngine::ScreenHeight (); y += 5)
   {
-    for (int32_t x = 0; x < olc::PixelGameEngine::ScreenHeight (); ++x)
+    for (int32_t x = 0; x < olc::PixelGameEngine::ScreenWidth (); ++x)
       olc::PixelGameEngine::Draw (x, static_cast<int32_t> (y - 80.0 / (1.0 + std::pow (x - 150, 4) / 8e6) * noise_.GetValue (x / 30.0 + frame_count_i / 50.0 + y, 0.0, 0.0)));
     //olc::PixelGameEngine::Draw (olc::PixelGameEngine::ScreenHeight () - 1, static_cast<int32_t> (1e4));
   } // end FOR
