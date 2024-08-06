c00ba9cb40e44dc59f900644ddcbe7a780760e65
Erik Sohns <erik.sohns@posteo.de>
2024-08-06 10:09:22 +0200

    minor changes

--- main_11.cpp
+++ main_11.cpp
@@ -1492,12 +1492,12 @@ ACE_TMAIN (int argc_in,
     log_file_name =
       Common_Log_Tools::getLogFilename (ACE_TEXT_ALWAYS_CHAR (engine_PACKAGE_NAME),
                                         ACE_TEXT_ALWAYS_CHAR (ACE::basename (argv_in[0])));
-  if (!Common_Log_Tools::initializeLogging (ACE_TEXT_ALWAYS_CHAR (ACE::basename (argv_in[0])), // program name
-                                            log_file_name,              // log file name
-                                            false,                      // log to syslog ?
-                                            false,                      // trace messages ?
-                                            trace_information,          // debug messages ?
-                                            NULL))                      // logger ?
+  if (!Common_Log_Tools::initialize (ACE_TEXT_ALWAYS_CHAR (ACE::basename (argv_in[0])), // program name
+                                     log_file_name,              // log file name
+                                     false,                      // log to syslog ?
+                                     false,                      // trace messages ?
+                                     trace_information,          // debug messages ?
+                                     NULL))                      // logger ?
   {
     ACE_DEBUG ((LM_ERROR,
                 ACE_TEXT ("failed to Common_Log_Tools::initializeLogging(), aborting\n")));
@@ -1576,7 +1576,7 @@ ACE_TMAIN (int argc_in,
   result = EXIT_SUCCESS;
 
 clean:
-  Common_Log_Tools::finalizeLogging ();
+  Common_Log_Tools::finalize ();
 
   // *PORTABILITY*: on Windows, finalize ACE
 #if defined (ACE_WIN32) || defined (ACE_WIN64)
