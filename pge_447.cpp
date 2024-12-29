e36a77ae1cde000755197d5cdcbbb1d5a4de18f8
Erik Sohns <erik.sohns@posteo.de>
2024-12-29 11:36:01 +0100

    minor changes

--- pge_447.cpp
+++ pge_447.cpp
@@ -4,9 +4,11 @@
 
 #include "ace/Log_Msg.h"
 
-#include "common_tools.h"
-
 #include "common_gl_tools.h"
+
+#include "common_image_tools.h"
+
+#include "common_math_tools.h"
 
 #include "engine_common.h"
 
@@ -14,7 +16,6 @@
 
 PGE_447::PGE_447 ()
  : olc::PixelGameEngine ()
- , colors_ ()
  , noise_ ()
 {
   sAppName = "Example 447";
@@ -25,7 +26,13 @@ PGE_447::OnUserCreate ()
 {
   olc::PixelGameEngine::SetPixelMode (olc::Pixel::ALPHA);
 
-  initializeColors ();
+#if defined (ACE_WIN32) || defined (ACE_WIN64)
+  tp1_ = std::chrono::high_resolution_clock::now ();
+#elif defined (ACE_LINUX)
+  tp1_ = std::chrono::high_resolution_clock::now ();
+#else
+#error missing implementation, aborting
+#endif // ACE_WIN32 || ACE_WIN64 || ACE_LINUX
 
   return true;
 }
@@ -33,126 +40,114 @@ PGE_447::OnUserCreate ()
 bool
 PGE_447::OnUserUpdate (float fElapsedTime)
 {
-  static int frame_count_i = 1;
-
   int pixels =
     olc::PixelGameEngine::GetDrawTargetWidth () * olc::PixelGameEngine::GetDrawTargetHeight ();
   olc::Pixel* p = olc::PixelGameEngine::GetDrawTarget ()->GetData ();
   for (int i = 0; i < pixels; i++)
     p[i].a = (p[i].a > ENGINE_PGE_447_DEFAULT_ALPHA_DECAY ? p[i].a - ENGINE_PGE_447_DEFAULT_ALPHA_DECAY : 0);
 
-  for (int i = 0; i < ENGINE_PGE_447_DEFAULT_NUMBER_OF_PLOTTERS; i++)
+#if defined (ACE_WIN32) || defined (ACE_WIN64)
+  std::chrono::steady_clock::time_point tp2 =
+    std::chrono::high_resolution_clock::now ();
+#elif defined (ACE_LINUX)
+  std::chrono::time_point<std::chrono::system_clock, std::chrono::nanoseconds> tp2 =
+    std::chrono::high_resolution_clock::now ();
+#else
+#error missing implementation, aborting
+#endif // ACE_WIN32 || ACE_WIN64 || ACE_LINUX
+  std::chrono::duration<float> elapsed_time = tp2 - tp1_;
+  std::chrono::milliseconds d =
+    std::chrono::duration_cast<std::chrono::milliseconds> (elapsed_time);
+	float t = d.count () / 3000.0f;
+
+  float r, g, b;
+  Common_Image_Tools::HSVToRGB (Common_GL_Tools::map (static_cast<float> (noise_.GetValue (100.0, t, 0.0)), -1.0f, 1.0f, 0.0f, 360.0f),
+                                0.3f,
+                                1.0f,
+                                r, g, b);
+  olc::Pixel color = olc::PixelF (r, g, b);
+
+  static int32_t screen_width_i = olc::PixelGameEngine::ScreenWidth ();
+  static int32_t screen_height_i = olc::PixelGameEngine::ScreenHeight ();
+  static olc::vf2d center_s (screen_width_i / 2.0f, screen_height_i / 2.0f);
+  static float radius_f = std::min (screen_width_i, screen_height_i) * 0.4f;
+  std::vector<olc::vf2d> poly = makePolygon (center_s, radius_f, 6);
+	for (int i = 0; i < 6; i++)
   {
-    int32_t x = Common_Tools::getRandomNumber (0, olc::PixelGameEngine::ScreenWidth () - 1);
-    int32_t y = Common_Tools::getRandomNumber (0, olc::PixelGameEngine::ScreenHeight () - 1);
-    int32_t row = static_cast<int32_t> (std::floor (y / (ENGINE_PGE_447_DEFAULT_SIZE / 2.0f)));
-    int32_t column = static_cast<int32_t> (std::floor (x / (ENGINE_PGE_447_DEFAULT_SIZE / 2.0f)));
-    olc::vf2d center ((column * 2 + 1) * ENGINE_PGE_447_DEFAULT_SIZE / 2.0f / 2.0f,
-                      (row    * 2 + 1) * ENGINE_PGE_447_DEFAULT_SIZE / 2.0f / 2.0f);
-    float distanceToCenter = std::hypot (x - center.x, y - center.y);
-    float directionToCenter = std::atan2 (x - center.x, y - center.y);
-    float nearestBranchAngle =
-      (std::round ((directionToCenter * ENGINE_PGE_447_DEFAULT_SYMMETRIES) / (2.0f * static_cast<float> (M_PI))) * (2.0f * static_cast<float> (M_PI)) / ENGINE_PGE_447_DEFAULT_SYMMETRIES);
-    float angleToNearestBranch = directionToCenter - nearestBranchAngle;
-    distanceToCenter *= std::cos (angleToNearestBranch);
-    double distanceLike = std::log (distanceToCenter / 3.0);
-
-    float distanceToNearestBranch = std::sin (angleToNearestBranch);
-    double angleLike = std::round (distanceToNearestBranch * 5.0) / 5.0;
-
-    double areaSeed = row * 13.0 + column * 41.0;
-    double timeLike = frame_count_i * ENGINE_PGE_447_DEFAULT_SPEED + areaSeed;
-
-    float val =
-      Common_GL_Tools::map (static_cast<float> (noise_.GetValue (distanceLike, angleLike, timeLike)), -1.0f, 1.0f, 0.0f, 1.0f);
-
-    static float maxDrawingRadius = ENGINE_PGE_447_DEFAULT_SIZE / 2.0f / 2.0f * 0.6f;
-    float drawingRadius =
-      maxDrawingRadius * std::min (1.0f, (frame_count_i % ENGINE_PGE_447_DEFAULT_CYCLE_DURATION) / (ENGINE_PGE_447_DEFAULT_CYCLE_DURATION / 3.0f));
-
-    float bloomingFactor = std::min (1.0f, 2.0f * distanceToCenter / drawingRadius);
-    val *= bloomingFactor;
-
-    struct plotter plotter = getPlotter (val);
-
-    static float branchLimit = 0.02f;
-    if (std::abs (distanceToNearestBranch) < branchLimit)
+		float base = Common_GL_Tools::map (static_cast<float> (noise_.GetValue (i * 2.0, t, 0.0)), -1.0f, 1.0f, 0.2f, 0.9f);
+		float hgt = Common_GL_Tools::map (static_cast<float> (noise_.GetValue (i * 2.0 + 1.0, t, 0.0)), -1.0f, 1.0f, 0.2f, 0.8f);
+    auto insertSpike = [base, hgt](olc::vf2d& a, olc::vf2d& b)
     {
-      plotter.color = colors_.color3;
-      plotter.radius = 2;
-    } // end IF
-    if (distanceToCenter > drawingRadius)
-      plotter.color.a = static_cast<uint8_t> (255.0f - 5.0f * (distanceToCenter - drawingRadius));
+      olc::vf2d v = b - a;
+      float vmag = v.mag ();
+      olc::vf2d vnorm (-v.y, v.x);
+      vnorm *= hgt;
+      float k1 = 0.5f - base / 2.0f;
+      olc::vf2d p1 = a.lerp (b, k1);
+      float k2 = 0.5f + base / 2.0f;
+      olc::vf2d p2 = a.lerp (b, k2);
+      olc::vf2d mid = a.lerp (b, 0.5f);
 
-    // At last, actually plot the dot.
-    olc::PixelGameEngine::FillCircle (x, y, plotter.radius, plotter.color);
-  } // end FOR
+      std::vector<olc::vf2d> result = {a, p1, mid + vnorm, p2, b};
+      return result;
+    };
+    auto insertSlab = [base, hgt](olc::vf2d& a, olc::vf2d& b)
+    {
+      olc::vf2d v = b - a;
+      float vmag = v.mag ();
+      olc::vf2d vnorm (-v.y, v.x);
+      vnorm *= hgt;
+      float k1 = 0.5f - base / 2.0f;
+      olc::vf2d p1 = a.lerp (b, k1);
+      float k2 = 0.5f + base / 2.0f;
+      olc::vf2d p2 = a.lerp (b, k2);
+      //olc::vf2d mid = a.lerp (b, 0.5f);
 
-  ++frame_count_i;
+      std::vector<olc::vf2d> result = {a, p1, p1 + vnorm, p2 + vnorm, p2, b};
+      return result;
+    };
+    
+    poly = applyToEveryEdge (poly, insertSpike);
+	} // end IF
+
+  for (std::vector<olc::vf2d>::iterator iterator = poly.begin();
+       iterator != poly.end ();
+       ++iterator)
+    olc::PixelGameEngine::Draw (*iterator, color);
 
   return !olc::PixelGameEngine::GetKey (olc::Key::ESCAPE).bPressed;
 }
 
-void
-PGE_447::initializeColors ()
+std::vector<olc::vf2d>
+PGE_447::makePolygon (olc::vf2d& center_in, float radius_in, int sides_in)
 {
-  static olc::Pixel pink (255, 143, 248);
-  static olc::Pixel blue1 (70, 198, 255);
-  static olc::Pixel blue2 (45, 121, 253);
-  static olc::Pixel blue3 (1, 43, 210);
+  std::vector<olc::vf2d> result;
 
-  colors_.color1 = blue3;
-  colors_.color2 = blue2;
-  colors_.color3 = pink;
+  olc::vf2d v (0.0f, -radius_in);
+  float dang = 2.0f * static_cast<float> (M_PI) / sides_in;
+  for (int i = 0; i < sides_in; i++)
+  {
+    result.push_back (center_in + v);
 
-  colors_.background = olc::BLACK;
+    float x_rot = v.x * std::cos (dang) - v.y * std::sin (dang);
+    float y_rot = v.x * std::sin (dang) + v.y * std::cos (dang);
+    v = {x_rot, y_rot};
+  } // end FOR
+
+  return result;
 }
 
-struct PGE_447::plotter
-PGE_447::getPlotter (float value_in)
+std::vector<olc::vf2d>
+PGE_447::applyToEveryEdge (std::vector<olc::vf2d>& pts, std::function<std::vector<olc::vf2d> (olc::vf2d&, olc::vf2d&)> f)
 {
-  static olc::Pixel palette_a[ENGINE_PGE_447_DEFAULT_PALETTE_SIZE] = {
-    colors_.background,
-    colors_.background,
-    colors_.background,
-    colors_.color1,
-    colors_.background,
-    colors_.background,
-    colors_.color2,
-    colors_.background,
-    colors_.background,
-    colors_.color3,
-    colors_.background,
-    colors_.background,
-    colors_.color1,
-    colors_.background,
-    colors_.background,
-    colors_.color2,
-    colors_.background,
-    colors_.background,
-    colors_.color3,
-    colors_.background,
-    colors_.background,
-    colors_.color1,
-    colors_.background,
-    colors_.background,
-    colors_.background,
-  };
-  static float size = 1.0f / ENGINE_PGE_447_DEFAULT_PALETTE_SIZE;
+  std::vector<olc::vf2d> result_a;
 
-  int index = static_cast<int> (std::floor (value_in / size));
-  if (index < 0)
-    index = 0;
-  else if (index > ENGINE_PGE_447_DEFAULT_PALETTE_SIZE - 1)
-    index = ENGINE_PGE_447_DEFAULT_PALETTE_SIZE - 1;
+  int n = static_cast<int> (pts.size ());
+  for (int i = 0; i < n; i++)
+  {
+    std::vector<olc::vf2d> sub_result_a = f (pts[i], pts[(i + 1) % n]);
+    result_a.insert (result_a.end (), sub_result_a.begin (), sub_result_a.end ());
+  } // end FOR
 
-  float valueInInterval = (value_in - (index * size)) / size;
-  static float center_f = 0.5f;
-  float radiusScale = 2.0f * (center_f - std::abs (valueInInterval - center_f));
-
-  struct plotter plotter_s;
-  plotter_s.color = palette_a[index];
-  plotter_s.radius = static_cast<int32_t> (ENGINE_PGE_447_DEFAULT_MAX_RADIUS * radiusScale / 2.0f);
-
-  return plotter_s;
+  return result_a;
 }
--- pge_447.h
+++ pge_447.h
@@ -1,6 +1,10 @@
 #ifndef PGE_447_H
 #define PGE_447_H
 
+#include <chrono>
+#include <functional>
+#include <vector>
+
 #include "noise/noise.h"
 
 #include "olcPixelGameEngine.h"
@@ -17,26 +21,18 @@ class PGE_447
   virtual bool OnUserUpdate (float); // elapsed time
 
  private:
-  struct colors
-  {
-    olc::Pixel color1;
-    olc::Pixel color2;
-    olc::Pixel color3;
+  std::vector<olc::vf2d> makePolygon (olc::vf2d&, float, int);
+  std::vector<olc::vf2d> applyToEveryEdge (std::vector<olc::vf2d>&, std::function<std::vector<olc::vf2d> (olc::vf2d&, olc::vf2d&)>);
 
-    olc::Pixel background;
-  };
-
-  struct plotter
-  {
-    int32_t    radius;
-    olc::Pixel color;
-  };
-
-  void initializeColors ();
-  struct plotter getPlotter (float);
-
-  struct colors         colors_;
   noise::module::Perlin noise_;
+  // time
+#if defined (ACE_WIN32) || defined (ACE_WIN64)
+  std::chrono::steady_clock::time_point tp1_;
+#elif defined (ACE_LINUX)
+  std::chrono::time_point<std::chrono::system_clock, std::chrono::nanoseconds> tp1_;
+#else
+#error missing implementation, aborting
+#endif // ACE_WIN32 || ACE_WIN64 || ACE_LINUX
 };
 
 #endif // PGE_447_H
