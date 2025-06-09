e342227348f8b12b7808e8977f4ca236115c3038
Erik Sohns <erik.sohns@posteo.de>
2025-06-09 20:03:45 +0200

    minor changes

--- defines_39.h
+++ defines_39.h
@@ -96,4 +96,9 @@
 #define ENGINE_GLUT_825_DEFAULT_HEIGHT             834
 #define ENGINE_GLUT_825_DEFAULT_WIDTH              1112
 
+// glut 826
+#define ENGINE_GLUT_826_DEFAULT_SCALE_FACTOR       30
+#define ENGINE_GLUT_826_DEFAULT_HEIGHT             834
+#define ENGINE_GLUT_826_DEFAULT_WIDTH              1112
+
 #endif // DEFINES_39_H
--- main_39.cpp
+++ main_39.cpp
@@ -63,6 +63,7 @@
 #include "glut_823.h"
 #include "glut_824.h"
 #include "glut_825.h"
+#include "glut_826.h"
 
 enum Engine_ModeType
 {
@@ -82,6 +83,7 @@ enum Engine_ModeType
   ENGINE_MODE_823,
   ENGINE_MODE_824,
   ENGINE_MODE_825,
+  ENGINE_MODE_826,
   ////////////////////////////////////////
   ENGINE_MODE_MAX,
   ENGINE_MODE_INVALID
@@ -2667,6 +2669,94 @@ do_work (int argc_in,
 
       if (!cb_data_s.shader.loadFromFile (ACE_TEXT_ALWAYS_CHAR ("glut_825.vert"),
                                           ACE_TEXT_ALWAYS_CHAR ("glut_825.frag")))
+      {
+         ACE_DEBUG ((LM_ERROR,
+                    ACE_TEXT ("failed to load shader, aborting\n")));
+         break;
+      } // end IF
+
+      cb_data_s.shader.use ();
+      cb_data_s.resolutionLoc =
+        glGetUniformLocation (cb_data_s.shader.id_, ACE_TEXT_ALWAYS_CHAR ("iResolution"));
+      ACE_ASSERT (cb_data_s.resolutionLoc != -1);
+      cb_data_s.timeLoc =
+        glGetUniformLocation (cb_data_s.shader.id_, ACE_TEXT_ALWAYS_CHAR ("iTime"));
+      ACE_ASSERT (cb_data_s.timeLoc != -1);
+
+      // START TIMING
+      cb_data_s.tp1 = std::chrono::high_resolution_clock::now ();
+
+      glutMainLoop ();
+
+      cb_data_s.shader.reset ();
+
+      result = true;
+
+      break;
+    }
+    case ENGINE_MODE_826:
+    {
+      struct Engine_OpenGL_GLUT_826_CBData cb_data_s;
+
+      cb_data_s.scaleFactor = ENGINE_GLUT_826_DEFAULT_SCALE_FACTOR;
+      cb_data_s.columns = ENGINE_GLUT_826_DEFAULT_WIDTH / cb_data_s.scaleFactor;
+      cb_data_s.rows = ENGINE_GLUT_826_DEFAULT_HEIGHT / cb_data_s.scaleFactor;
+
+      cb_data_s.resolutionLoc = -1;
+      cb_data_s.timeLoc = -1;
+
+      cb_data_s.wireframe = false;
+
+      cb_data_s.mouseX = ENGINE_GLUT_826_DEFAULT_WIDTH / 2;
+      cb_data_s.mouseY = ENGINE_GLUT_826_DEFAULT_HEIGHT / 2;
+      cb_data_s.mouseLMBPressed = false;
+
+      // initialize GLUT
+      glutInit (&argc_in, argv_in);
+      glutSetOption (GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+      glutInitDisplayMode (GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH);
+      glutInitWindowSize (ENGINE_GLUT_826_DEFAULT_WIDTH, ENGINE_GLUT_826_DEFAULT_HEIGHT);
+
+      int window_i = glutCreateWindow ("engine GLUT 826");
+      glutSetWindow (window_i);
+      glutSetWindowData (&cb_data_s);
+
+      // initialize GLEW
+      GLenum err = glewInit ();
+      if (GLEW_OK != err)
+      {
+        ACE_DEBUG ((LM_ERROR,
+                    ACE_TEXT ("failed to glewInit(): \"%s\", aborting\n"),
+                    ACE_TEXT (glewGetErrorString (err))));
+        break;
+      } // end IF
+      ACE_DEBUG ((LM_DEBUG,
+                 ACE_TEXT ("using GLEW version: %s\n"),
+                 ACE_TEXT (glewGetString (GLEW_VERSION))));
+
+      glClearColor (0.0f, 0.0f, 0.0f, 1.0f);
+
+      glPolygonMode (GL_FRONT_AND_BACK, GL_FILL);
+
+      glutDisplayFunc (engine_glut_826_draw);
+      glutReshapeFunc (engine_glut_826_reshape);
+      glutVisibilityFunc (engine_glut_826_visible);
+
+      glutKeyboardFunc (engine_glut_826_key);
+      glutKeyboardUpFunc (engine_glut_826_key_up);
+      glutSpecialFunc (engine_glut_826_key_special);
+      glutMouseFunc (engine_glut_826_mouse_button);
+      glutMotionFunc (engine_glut_826_mouse_move);
+      glutPassiveMotionFunc (engine_glut_826_mouse_move);
+
+      glutTimerFunc (100, engine_glut_826_timer, 0);
+
+      glutCreateMenu (engine_glut_826_menu);
+      glutAddMenuEntry (ACE_TEXT_ALWAYS_CHAR ("wireframe"), 0);
+      glutAttachMenu (GLUT_RIGHT_BUTTON);
+
+      if (!cb_data_s.shader.loadFromFile (ACE_TEXT_ALWAYS_CHAR ("glut_826.vert"),
+                                          ACE_TEXT_ALWAYS_CHAR ("glut_826.frag")))
       {
          ACE_DEBUG ((LM_ERROR,
                     ACE_TEXT ("failed to load shader, aborting\n")));
